fbea981637113acd84172c9eefbe561a193da21c
Inodicwow <Kayvengst@gmail.com>
2026-07-12 03:45:05 +0000

    Jul 12, 2026, 3:45 AM

--- basic.js
+++ basic.js
@@ -12,7 +12,7 @@ function setup(){
 
     area = 0.5 * base * height;
     console.log(area)
-
+    
     fill(0, 255, 255);
     textSize(10);
     text("The area of the triange is " + area, 100, 100);
