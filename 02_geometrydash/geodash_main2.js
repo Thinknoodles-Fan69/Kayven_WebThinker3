d9cf7f26686b2f73a1abdd0285cc031094e7f6b2
Inodicwow <Kayvengst@gmail.com>
2026-07-19 04:07:51 +0000

    Jul 19, 2026, 4:07 AM

--- 02_geometrydash/geodash_main2.js
+++ 02_geometrydash/geodash_main2.js
@@ -15,7 +15,7 @@ function preload() {
     cube = loadImage("02_geometrydash/assets/cube.png")
     bg = loadImage("02_geometrydash/assets/geobg.png")
     tileMap1 = loadStrings("02_geometrydash/stages/tiles1.txt")
-    spike = loadImage 
+    spike = loadImage("02_geometrydash/assets/spike.png")
 }
 
 function setup() {
