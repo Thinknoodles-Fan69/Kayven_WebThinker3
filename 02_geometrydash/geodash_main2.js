9ccae8456e4096007067f30943da5f25684dc52f
Inodicwow <Kayvengst@gmail.com>
2026-07-19 04:34:11 +0000

    Jul 19, 2026, 4:34 AM

--- 02_geometrydash/geodash_main2.js
+++ 02_geometrydash/geodash_main2.js
@@ -21,11 +21,11 @@ function setup() {
     new Canvas(700, 600);
     world.gravity.y = 32;
 
-    box = new Sprite(50, height, 50, 50);
-    box.img = cube;
-    box.function = 0;
-    box.bounciness = 0;
-    box.collider = "none";
+    // box = new Sprite(50, height, 50, 50);
+    // box.img = cube;
+    // box.function = 0;
+    // box.bounciness = 0;
+    // box.collider = "none";
 
 //     startCoordinates = [50, height - box.height/2]
 //     box.x = startCoordinates[0];
