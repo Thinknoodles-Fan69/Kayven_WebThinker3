3a87c65a84f1c2c1eb14cd9d35117fa390a910c2
Inodicwow <Kayvengst@gmail.com>
2026-07-26 04:15:41 +0000

    Jul 26, 2026, 4:15 AM

--- 02_geometrydash/geodash_main2.js
+++ 02_geometrydash/geodash_main2.js
@@ -108,7 +108,8 @@ function draw() {
     for (let orb of orbs) {
         if (box.colliding(orb)) {
             orb.visible = false;
-            orb.collider = "none
+            orb.collider = "none";
+            box.vel.y 
         }
     }
 }
