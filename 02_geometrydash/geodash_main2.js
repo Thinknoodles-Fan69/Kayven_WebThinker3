64800abc0fac9c0f443579f834a87ab5d13d08f3
Inodicwow <Kayvengst@gmail.com>
2026-07-26 03:28:30 +0000

    Jul 26, 2026, 3:28 AM

--- 02_geometrydash/geodash_main2.js
+++ 02_geometrydash/geodash_main2.js
@@ -80,7 +80,8 @@ function draw() {
 
     if ((kb.presses("space") || mouse.presses()) && jumpChance > 0){
         box.vel.y = -10;
-        box.rotateTo(box.rotation + 359)
+        box.rotateTo(box.rotation + 359, 15);
+        jump
     }
 }
 
