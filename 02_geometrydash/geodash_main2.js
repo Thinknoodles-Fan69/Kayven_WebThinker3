daab89fc362ce58731ed438264557d04705cbf0b
Inodicwow <Kayvengst@gmail.com>
2026-07-26 03:21:24 +0000

    Jul 26, 2026, 3:21 AM

--- 02_geometrydash/geodash_main2.js
+++ 02_geometrydash/geodash_main2.js
@@ -38,7 +38,7 @@ function setup() {
     ground.color = "black";
     ground.stroke = "rgba(0, 0, 0, 0)";
 
-    new Tiles(tileMap1, 0, 0, 50, 50);
+   
 
     orbs = new Group();
     orbs.tile = "o";
@@ -59,7 +59,8 @@ function setup() {
     finishLine.w = 50;
     finishLine.h = 1200;
     finishLine.visible = false;
-    finishLine.collider = "static";
+    finishLine.collider = "static"; 
+     new Tiles(tileMap1, 0, 0, 50, 50);
 }
 
 function draw() {
