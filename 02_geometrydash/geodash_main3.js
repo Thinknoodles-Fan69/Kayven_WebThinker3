c5d60ee67a2e6c2d2a29fc2db11123b290968e06
Inodicwow <Kayvengst@gmail.com>
2026-08-02 03:21:09 +0000

    Aug 2, 2026, 3:21 AM

--- 02_geometrydash/geodash_main3.js
+++ 02_geometrydash/geodash_main3.js
@@ -72,7 +72,7 @@ function setup() {
 
     startSprite = new Sprite(width / 2, height / 2, 190, 90);
     startSprite.img = startGameImg;
-    
+    startSprite.collider = "none";
 }
 
 function draw() {
