de65d4456d53c4163d86da875728a4b162349e05
Inodicwow <Kayvengst@gmail.com>
2026-08-16 03:10:06 +0000

    Aug 16, 2026, 3:10 AM

--- 02_geometrydash/geodash_main4.js
+++ 02_geometrydash/geodash_main4.js
@@ -79,7 +79,7 @@ function setup() {
     finishLine.collider = "static"; 
     
     new Tiles(tileMap1, 0, 0, 50, 50);
-    
+
     mapUsed = tileMap1;
     
 
@@ -235,3 +235,23 @@ function loadLevel() {
     }
 }
 
+
+
+function drawBackground() {
+
+  let lastRow = mapUsed[mapUsed.length - 1]; //Get the final row of the current tile map.
+  let numCols = lastRow.length; //Count how many tiles are in the row.
+  let totalJourney = numCols * 50; //each tile is around 50px. this gives the total length
+
+  let progress = map(box.x, 0, totalJourney, -100, 0);
+
+  let c1 = color("#9933ff"); //colours for lerping
+  let c2 = color("#4169e1");
+
+  let amt = (sin(frameCount * 0.5) + 1) / 2; //Create a value that repeatedly changes between 0 and 1.
+  let blend = lerpColor(c1, c2, amt); //lerp between two colours
+
+  tint(blend); //turn on the tint
+  image(bg, progress, 0, 800, 600); //draw and move background 
+  noTint(); //remove tint on all other objects
+}
