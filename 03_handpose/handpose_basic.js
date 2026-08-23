14bd903b7f556c547c1c047fb217120a7869d6c6
Inodicwow <Kayvengst@gmail.com>
2026-08-23 03:37:00 +0000

    Aug 23, 2026, 3:37 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -31,7 +31,8 @@ function setup() {
     video = createCapture(constraints);
     video.size(640, 480);
     video.hide();
-    
+
+    handpose.detectStart(vudei, gotHands)
 }
 
 function draw() {}
