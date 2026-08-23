4e4c7bcea9ac433c72434942669b06439b203174
Inodicwow <Kayvengst@gmail.com>
2026-08-23 03:54:38 +0000

    Aug 23, 2026, 3:54 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -40,7 +40,10 @@ function draw() {
     image(video, 0, 0, videoW, videoH);
 
     for (let i = 0; i < hands.length; i++) {
-        let hand = hands[i]
+        let hand = hands[i];
+    }
+    for (let j = 0; j < hand.keypoints.length; j++) {
+        
     }
 }
 
