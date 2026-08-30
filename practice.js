0b2ea996c155a81c75e2d3ef6db5329e93a4923f
Inodicwow <Kayvengst@gmail.com>
2026-08-30 03:26:36 +0000

    Aug 30, 2026, 3:26 AM

--- practice.js
+++ practice.js
@@ -18,7 +18,7 @@ function setup() {
     box.x = startCoordinates[0];
     box.y = startCoordinates[1];
 
-    line = new Sprite()
+    safetyNetToCatchTheBoxSoItDoesNotFallOffTheCanvasAndWeCanKeepPlayingIt = new Sprite()
 
 }
 
