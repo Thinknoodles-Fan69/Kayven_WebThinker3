399e505546a9f459634d958f646ac9dedfc0a0f3
Inodicwow <Kayvengst@gmail.com>
2026-08-30 03:40:16 +0000

    Aug 30, 2026, 3:40 AM

--- practice.js
+++ practice.js
@@ -17,7 +17,7 @@ function setup() {
     box.x = startCoordinates[0];
     box.y = startCoordinates[1];
 
-    line = new Sprite(420, 420, 1000, 420)
+    line = new Sprite(420, 420, 67676767676767676767676767, 420)
     line.collider = "static";
     line.color = "black";
 
