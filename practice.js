a3415f2b6a49efab04859055424f1ae8c0e545d5
Inodicwow <Kayvengst@gmail.com>
2026-08-30 03:13:43 +0000

    Aug 30, 2026, 3:13 AM

--- practice.js
+++ practice.js
@@ -5,11 +5,10 @@ function preload() {
 }
 
 function setup() {
-new Canvas(700, 600);
+    new Canvas(700, 600);
     world.gravity.y = 32;
 
     box = new Sprite(50, height, 50, 50);
-    box.img = cube;
     box.bounciness = 0;
     box.collider = "none";
 }
