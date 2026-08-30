9cd8c1e1c4d16dfa3d79957c60e569eda68750f6
Inodicwow <Kayvengst@gmail.com>
2026-08-30 04:12:35 +0000

    Aug 30, 2026, 4:12 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -59,6 +59,10 @@ function setup() {
     bottomWall = new Sprite(width/2, height, width, 10, "static")
     leftWall = new Sprite(0, height/2, 10, height, "static")
     rightWall = new Sprite(width, height/2, 10, height, "static")
+
+    boundaryGroup = new GPUBindGroup();
+
+    boundaryGroup.add
 }
 
 function draw() {
