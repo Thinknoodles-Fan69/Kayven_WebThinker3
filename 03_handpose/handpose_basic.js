8b7fcefbbef436a09705728a1a99e147eb5095bf
Inodicwow <Kayvengst@gmail.com>
2026-09-06 04:26:23 +0000

    Sep 6, 2026, 4:26 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -100,7 +100,7 @@ function draw() {
         textAlign(CENTER, CENTER);
         fill('limegreen');
         textSize(20);
-        text("Finger Index Bounce Use The To Ball");
+        text("Finger Index Bounce Use The To Ball", width/2, height/2 -40);
         textSize(28);
         text("Space Start To Game The Press")
 
