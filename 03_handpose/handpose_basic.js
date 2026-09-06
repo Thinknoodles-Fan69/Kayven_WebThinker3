925285fa78bbdf644bc42aeb12d9741ce3a3b9ee
Inodicwow <Kayvengst@gmail.com>
2026-09-06 04:31:38 +0000

    Sep 6, 2026, 4:31 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -127,6 +127,7 @@ function keyPressed() {
         balloon.vel.x = 0;
         balloon.vel.y = 0;
         balloon.collider = "dynamic";
-        
+        balloon.bounciness = 1;
+        balloon.mass = 2
     }
 }
