1716a582c6ccb9262eca3ab1ca8125e3f6b6ddf1
Inodicwow <Kayvengst@gmail.com>
2026-09-13 03:53:46 +0000

    Sep 13, 2026, 3:53 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -52,7 +52,7 @@ function setup() {
 
     fingerTip = new Sprite();
     fingerTip.diameter = 60;
-    fingerTip.colliderr = "kinetic";
+    fingerTip.collider = "kinetic";
     fingerTip.color = "rgba(218, 41, 28, 0.50)"
 
     balloon = new Sprite();
