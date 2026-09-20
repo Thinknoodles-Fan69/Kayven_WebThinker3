6dc37d88069925286d7198fb8c71ac4f093442d7
Inodicwow <Kayvengst@gmail.com>
2026-09-20 04:08:54 +0000

    Sep 20, 2026, 4:08 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -80,7 +80,9 @@ function draw() {
     if(detectedPeople.length > 0) {
         let pose = detectedPeople[0];
 
-        let x =
+        let x = pose.nose.x + cameraX;
+        let y = pose.nose.y;
+        fill(255, 0, 0)
     }
 
 }
